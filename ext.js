a9ab7120a6e5e17943d98a7cf4c030770a789ee0
Christian Avi Bulan <43099141+christianavi@users.noreply.github.com>
2021-01-29 12:53:32 +0800

    Create ext.js

---
+++ ext.js
@@ -0,0 +1,23 @@
+function ext() {
+
+    var ads = [
+        ".searchCenterTopAds", ".searchRightBottomAds", ".searchCenterBottomAds",
+        "#taw", "#bottomads",
+        ".b_ad", ".b_adBottom"
+    ]
+
+    console.log("[Vanilla] Attempting to remove ads from " + window.location.host) 
+    for (let i = 0; i < ads.length; i++) {
+        if (ads[i]) {
+            try {
+                document.querySelector(ads[i]).style.display = "none"
+                console.log("[Vanilla] " + ads[i] + " is cleared.")
+            } catch (error) {
+                console.error(error);
+            }
+        }       
+    }
+    console.log("[Vanilla] Success.")
+}
+
+export { ext }
